8411f736787ee8967d64dcc03019052875ebe18d
kcd <kcd@14.32.152.212>
2018-10-06 02:06:01 +0900

    1

--- src/main/java/org/devframe/web/home/controller/HomeController.java
+++ src/main/java/org/devframe/web/home/controller/HomeController.java
@@ -1,16 +1,14 @@
 package org.devframe.web.home.controller;
 
-import org.devframe.web.sample01.vo.Sample01VO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/")
 public class HomeController {
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String list(Sample01VO sample01VO) {
+	public String index() {
 		return "home/homeIndex";
 	}
 
